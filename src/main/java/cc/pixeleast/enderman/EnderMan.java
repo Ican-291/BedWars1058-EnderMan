11e80e1f9670a7906c890c9894c4eb90468b87a4
Ican-291 <grass_20113@qq.com>
2024-12-14 10:27:49 +0800

    Start of everything

---
+++ src/main/java/cc/pixeleast/enderman/EnderMan.java
@@ -0,0 +1,16 @@
+package cc.pixeleast.enderman;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderMan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
